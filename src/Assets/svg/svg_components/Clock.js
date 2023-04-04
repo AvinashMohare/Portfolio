d6c73ab5ecdaec0deccc0f51ee01aed752504bf1
avinashmohare <mohareavinash80@gmail.com>
2023-04-04 16:31:20 +0530

    Main Page Done

---
+++ src/Assets/svg/svg_components/Clock.js
@@ -0,0 +1,51 @@
+const Clock = () => {
+    return (
+        <svg
+            width="15px"
+            height="15px"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#9aa0a6"
+        >
+            {/* <g id="SVGRepo_bgCarrier" stroke-width="0" /> */}
+
+            <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+
+            <g id="SVGRepo_iconCarrier">
+                <title />
+                <g id="Complete">
+                    <g id="Clock">
+                        <g>
+                            <polyline
+                                fill="none"
+                                points="11.9 5.9 11.9 11.9 12 12 14.1 14.1"
+                                stroke="#9aa0a6"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                            />
+                            <circle
+                                cx="12"
+                                cy="12"
+                                data-name="Circle"
+                                fill="none"
+                                id="Circle-2"
+                                r="10"
+                                stroke="#9aa0a6"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                            />
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+};
+
+export default Clock;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,17 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "../styles/Searchbar.module.scss";
-import Search from "@/Assets/svg/svg_components/search";
+import Search from "@/Assets/svg/svg_components/Search";
+import Voice from "@/Assets/svg/svg_components/Voice";
+import Camera from "@/Assets/svg/svg_components/Camera";
+import Clock from "@/Assets/svg/svg_components/Clock";
+
+const SearchItems = [
+    {
+        query: "Want to know more about the person behind this website",
+        link: "page_link",
+    },
+    {
+        query: "Explore my highlights and achievements",
+        link: "page_link",
+    },
+    {
+        query: "Browse my project portfolio",
+        link: "page_link",
+    },
+    {
+        query: "Curious about my work experience and expertise",
+        link: "page_link",
+    },
+    {
+        query: "Discover my personal interests and hobbies outside of work",
+        link: "page_link",
+    },
+    {
+        query: "Explore my academic background and qualifications",
+        link: "page_link",
+    },
+];
 
 const Searchbar = () => {
+    const [value, setValue] = useState("");
+    const [show, setShow] = useState(true);
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+        console.log(event.target.value);
+    };
+
     return (
-        <div className={classes.rootSearchbar}>
-            <div className={classes.searchbar}>
+        <div
+            className={`${classes.rootSearchbar} ${
+                show ? classes.active : classes.null
+            }`}
+        >
+            <div
+                className={`${classes.searchbar} ${
+                    show ? classes.active : classes.inactive
+                }`}
+            >
                 <div className={classes.searchIcon}>
                     <Search />
                 </div>
-                <input placeholder="Hii I am Avinash " />
+
+                <div className={classes.searchInner}>
+                    <input
+                        type="text"
+                        value={value}
+                        onChange={onChange}
+                        onFocus={() => {
+                            setShow(true);
+                        }}
+                        onBlur={() => setShow(false)}
+                        placeholder="Hii I am Avinash "
+                    />
+                </div>
+
+                <div className={classes.voice}>
+                    <Voice />
+                </div>
+                <div className={classes.camera}>
+                    <Camera />
+                </div>
             </div>
+
+            {show && (
+                <>
+                    <div className={classes.divider}></div>
+
+                    <div
+                        className={`${classes.dropdown} ${
+                            show ? classes.active : classes.null
+                        }`}
+                    >
+                        {SearchItems.map((item, index) => {
+                            return (
+                                <div className={classes.dropdownContainer}>
+                                    <div className={classes.clock}>
+                                        <Clock />
+                                    </div>
+
+                                    <p
+                                        className={classes.dropdownRow}
+                                        key={index}
+                                    >
+                                        {item.query}
+                                    </p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </>
+            )}
         </div>
     );
 };
