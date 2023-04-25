a6babab8feceac5fb358ff1839306867a2e01f3e
avinashmohare <mohareavinash80@gmail.com>
2023-04-26 00:00:26 +0530

    Done

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,12 +4,36 @@ import Image from "next/image";
 import Avinash from "../Assets/images/Avinash.jpg";
 import Landing from "./landing";
 
+const ProjectsList = [
+    {
+        title: "Mac",
+    },
+
+    {
+        title: "Win",
+    },
+];
+
 const Projects = () => {
     return (
         <div className={classes.rootProjects}>
             <div className={classes.heading}>
                 <p>Projects</p>
             </div>
+
+            {/* <div className={classes.content}>
+                <div className={classes.cards}>
+                    {ProjectsList.map((projects) => {
+                        return (
+                            <>
+                                <a>
+                                    <div className={classes.projectCard}></div>
+                                </a>
+                            </>
+                        );
+                    })}
+                </div>
+            </div> */}
         </div>
     );
 };
