1bb7b5e612993113e27e2578bf6eaa795c82f75c
avinashmohare <mohareavinash80@gmail.com>
2023-05-01 12:30:48 +0530

    Full Responsive website

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -8,7 +8,7 @@ import Twitter from "@/Assets/svg/svg_components/Twitter";
 
 function ContactForm() {
     const [state, handleSubmit] = useForm("xgebdwgo");
-    if (state.succeeded) {
+    if (false) {
         return (
             <div className={classes.Submitted}>
                 <p className={classes.text}>Thank you for contacting!</p>
